f56862e741526c84d124350d0a4c661875cc8086
Jeremy Echols <jechols@uoregon.edu>
2013-10-30 11:40:41 -0700

    Initial Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsFormBuilder::Application.config.secret_key_base = '78968faf2a55d52269f816a811e3e7d29db65f1aa734f7aa69c1efaf9436d4e6962ac06b969a1d6878313d825f37bf836e4606a9bd69ec30a644804d5a535d09'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsFormBuilder::Application.config.session_store :cookie_store, key: '_rails-form-builder_session'
