b42540fa90b2ba990d296738e2da9bc1c692ee8a
Jeremy Echols <jechols@uoregon.edu>
2013-11-05 13:19:28 -0800

    Move test classes into before block
    Avoids issues where modifying decorator at runtime causes future tests
    to fail (or have other unexpected results)

--- spec/lib/data_structure/container_spec.rb
+++ spec/lib/data_structure/container_spec.rb
@@ -3,59 +3,68 @@
 # Isolated testing for speed
 require_relative "../../../lib/data_structure/container"
 
-class TestDelegatee
-  def delegation
-    return 1
-  end
-
-  def to_s
-    return "This is TestDelegatee's to_s method"
-  end
-end
-
-class TestDecorator
-  include DataStructure::Container
-
-  sections :asset_metadata, :other_data
-
-  # Simplest case - attribute with no real rules
-  attribute :thing, section: :other_data
-
-  # Simple case - required attribute with no other rules
-  attribute :identifier, required: true, section: :other_data
-
-  # Semi-simple case - attribute with multiples allowed
-  attribute :subjects, required: true, section: :other_data, multiple: true
-
-  # Complex case - attribute with sub-types but no multiple values
-  attribute :option, required: true, section: :asset_metadata do |option|
-    option.subtype :first, delegation: :option_1
-    option.subtype :alternate, delegation: :option_2
-  end
-
-  # Complex case - attribute with sub-types and allowance for multiple values
-  attribute :titles, multiple: true, required: true, section: :asset_metadata do |title|
-    title.subtype :main, delegation: :main_title
-    title.subtype :alternate, delegation: :alt_title
-    title.subtype :parallel, delegation: :parallel_title
-    title.subtype :series, delegation: :series_name
-  end
-
-  # Local function operating on local data
-  def modify
-    @something ||= 0
-    @something += 1
-  end
-
-  # Another local function wheee
-  def get
-    return @something
-  end
-end
-
 describe DataStructure::Container do
   let(:decoratee) { TestDelegatee.new }
   subject { TestDecorator.new(decoratee) }
+
+  # This is VERY BAD.  Just need to make this work properly for prototyping,
+  # then I swear I'll fix it!
+  before(:each) do
+    class TestDelegatee
+      def delegation
+        return 1
+      end
+
+      def to_s
+        return "This is TestDelegatee's to_s method"
+      end
+    end
+
+    class TestDecorator
+      include DataStructure::Container
+
+      sections :asset_metadata, :other_data
+
+      # Simplest case - attribute with no real rules
+      attribute :thing, section: :other_data
+
+      # Simple case - required attribute with no other rules
+      attribute :identifier, required: true, section: :other_data
+
+      # Semi-simple case - attribute with multiples allowed
+      attribute :subjects, required: true, section: :other_data, multiple: true
+
+      # Complex case - attribute with sub-types but no multiple values
+      attribute :option, required: true, section: :asset_metadata do |option|
+        option.subtype :first, delegation: :option_1
+        option.subtype :alternate, delegation: :option_2
+      end
+
+      # Complex case - attribute with sub-types and allowance for multiple values
+      attribute :titles, multiple: true, required: true, section: :asset_metadata do |title|
+        title.subtype :main, delegation: :main_title
+        title.subtype :alternate, delegation: :alt_title
+        title.subtype :parallel, delegation: :parallel_title
+        title.subtype :series, delegation: :series_name
+      end
+
+      # Local function operating on local data
+      def modify
+        @something ||= 0
+        @something += 1
+      end
+
+      # Another local function wheee
+      def get
+        return @something
+      end
+    end
+  end
+
+  after(:each) do
+    Object.send(:remove_const, :TestDecorator) if Object.const_defined?(:TestDecorator)
+    Object.send(:remove_const, :TestDelegatee) if Object.const_defined?(:TestDelegatee)
+  end
 
   context "decoration magic" do
     it "should create a new decorated object" do
