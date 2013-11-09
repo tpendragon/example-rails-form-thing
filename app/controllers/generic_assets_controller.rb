ad6d482932cb8b27097a4b7f079e30277ad34b6f
Jeremy Echols <jechols@uoregon.edu>
2013-11-08 16:24:21 -0800

    Add generic asset structure decoration

--- app/controllers/generic_assets_controller.rb
+++ app/controllers/generic_assets_controller.rb
@@ -64,9 +64,21 @@ class GenericAssetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    # Get a structurized, decorated generic asset
+    #
+    # NOTE, this decoration is manual so it's clear what we're doing here.  We could easily use
+    # Draper's magic #decorate method, and do something similar with DataStructure.
     def set_generic_asset
+      # Base ORM object
       @generic_asset = GenericAsset.find(params[:id])
+
+      # Decorate it with our structure class
+      @generic_asset = GenericAssetStructure.new(@generic_asset)
+
+      # TODO: Decorate it with the draper view decorations once we have draper
+      # stuff.  Note that the draper decorations MUST be last due to all its
+      # weird magic!
+      #@generic_asset = GenericAssetViewDecorator.new(@generic_asset)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
