ac9b1d64529722c5337968ccef7c8bcc63d3b812
Jeremy Echols <jechols@uoregon.edu>
2013-11-15 11:15:54 -0800

    Refactor renderer spec to fix failures
    Stubs the required :attributes method on the tested model.  To avoid
    duplication, surrounds both class specs with a generalized "Renderers"
    describe block.

--- spec/lib/data_structure/renderer_spec.rb
+++ spec/lib/data_structure/renderer_spec.rb
@@ -2,39 +2,42 @@
 require_relative "../../../lib/data_structure/renderer"
 require_relative "../../../lib/data_structure/label_translator"
 
-describe DataStructure::Renderer do
-  let(:renderee) { double("model") }
-  let(:renderer) { DataStructure::Renderer.new(renderee) }
-
-  before(:each) do
-    # Make renderee act as if it has sections and such so all the internal API bits work
-    renderee.class.stub(:sections => [:foo, :bar])
-  end
-
-  describe ".new" do
-    it "should create a new DataStructure::Renderer wrapping the given object" do
-      expect(renderer.model).to eq(renderee)
-    end
-
-    it "should create section renderers" do
-      expect(renderer.section_renderers.length).to eq(2)
-      expect(renderer.section_renderers[:foo]).to be_kind_of(DataStructure::SectionRenderer)
-      expect(renderer.section_renderers[:bar]).to be_kind_of(DataStructure::SectionRenderer)
-    end
-  end
-end
-
-describe DataStructure::SectionRenderer do
+describe "Renderers" do
   let(:model) { double("model") }
-  subject { DataStructure::SectionRenderer.new(:name, model) }
+  before(:each) do
+    # Make model act as if it has sections and such so all the internal API bits work
+    model.class.stub(:sections => [:foo, :bar])
+    model.class.stub(:attributes => [])
+  end
 
-  describe "#label" do
-    it "should use LabelTranslator" do
-      lt = double("label translator instance")
-      DataStructure::LabelTranslator.should_receive(:new).with(model, :name).and_return(lt)
-      lt.stub(:translate => "translated")
 
-      expect(subject.label).to eq("translated")
+  describe DataStructure::Renderer do
+    let(:renderer) { DataStructure::Renderer.new(model) }
+
+    describe ".new" do
+      it "should create a new DataStructure::Renderer wrapping the given object" do
+        expect(renderer.model).to eq(model)
+      end
+
+      it "should create section renderers" do
+        expect(renderer.section_renderers.length).to eq(2)
+        expect(renderer.section_renderers[:foo]).to be_kind_of(DataStructure::SectionRenderer)
+        expect(renderer.section_renderers[:bar]).to be_kind_of(DataStructure::SectionRenderer)
+      end
+    end
+  end
+
+  describe DataStructure::SectionRenderer do
+    subject { DataStructure::SectionRenderer.new(:name, model) }
+
+    describe "#label" do
+      it "should use LabelTranslator" do
+        lt = double("label translator instance")
+        DataStructure::LabelTranslator.should_receive(:new).with(model, :name).and_return(lt)
+        lt.stub(:translate => "translated")
+
+        expect(subject.label).to eq("translated")
+      end
     end
   end
 end
